e7363175b2ed260d040e38ecf35fb1778ca83e17
Enk0d <D1man680@yandex.com>
2023-02-07 22:35:08 +0300

    Add models

--- web_interface/models.py
+++ web_interface/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    username = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=40)
+    name = models.CharField(max_length=50)
+    second_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+
+
+class Posts(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    source_link = models.URLField()
+    rss_link = models.ForeignKey(Req_Urls, on_delete=models.PROTECT)
+
+
+class Tags(models.Model):
+    tag = models.CharField(max_length=50, blank=False, unique=True)
+    users_tags = models.ManyToManyField(Users)
