05a3981ea629d0c28cd7ac987652990c2ada0caa
Enk0d <D1man680@yandex.com>
2023-02-07 23:22:03 +0300

    Add urls and views for pages

--- news_portal/news_portal/urls.py
+++ news_portal/news_portal/urls.py
@@ -15,10 +15,52 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from web_interface.views import main_page
+from web_interface.views import (
+    main_page,
+    Posts_ListView,
+    Users_ListView,
+    RSS_links_ListView,
+    Tags_ListView,
+    Posts_DetailView,
+    Users_DetailView,
+    RSS_links_DetailView,
+    Tags_DetailView,
+    Posts_Delete,
+    Users_Delete,
+    RSS_links_Delete,
+    Tags_Delete,
+    Posts_CreateView,
+    Users_CreateView,
+    RSS_links_CreateView,
+    Tags_CreateView,
+    Posts_UpdateView,
+    Users_UpdateView,
+    RSS_links_UpdateView,
+    Tags_UpdateView,
+)
+
 
 urlpatterns = [
     path('', main_page, name='main'),
-
+    path('posts/', Posts_ListView.as_view(), name='posts'),
+    path('posts/<int:pk>/', Posts_DetailView.as_view(), name='post'),
+    path('posts/create/', Posts_CreateView.as_view(), name='post_create'),
+    path('posts/change/<int:pk>/', Posts_UpdateView.as_view(), name='post_change'),
+    path('posts/delete/<int:pk>/', Posts_Delete.as_view(), name='post_delete'),
+    path('users/', Users_ListView.as_view(), name='users'),
+    path('users/<int:pk>/', Users_DetailView.as_view(), name='user'),
+    path('users/create/', Users_CreateView.as_view(), name='user_create'),
+    path('users/change/<int:pk>/', Users_UpdateView.as_view(), name='user_change'),
+    path('users/delete/<int:pk>/', Users_Delete.as_view(), name='user_delete'),
+    path('rss_links/', RSS_links_ListView.as_view(), name='sources'),
+    path('rss_links/<int:pk>/', RSS_links_DetailView.as_view(), name='source'),
+    path('rss_links/create/', RSS_links_CreateView.as_view(), name='source_create'),
+    path('rss_links/change/<int:pk>/', RSS_links_UpdateView.as_view(), name='source_change'),
+    path('rss_links/delete/<int:pk>/', RSS_links_Delete.as_view(), name='source_delete'),
+    path('tags/', Tags_ListView.as_view(), name='tags'),
+    path('tags/<int:pk>/', Tags_DetailView.as_view(), name='tag'),
+    path('tags/create/', Tags_CreateView.as_view(), name='tag_create'),
+    path('tags/change/<int:pk>/', Tags_UpdateView.as_view(), name='tag_change'),
+    path('tags/delete/<int:pk>/', Tags_Delete.as_view(), name='tag_delete'),
     path('admin/', admin.site.urls),
 ]
--- news_portal/web_interface/views.py
+++ news_portal/web_interface/views.py
@@ -1,6 +1,108 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, DetailView ,UpdateView, DeleteView
+from web_interface.models import Posts, Users, RSS_links, Tags
 
 # Create your views here.
 
 def main_page(request):
     return render(request, 'web_interface/index.html')
+
+class Posts_CreateView(CreateView):
+    model = Posts
+    fields = ('title', 'text', 'url', 'from_url')
+    #form_class = PostForm
+    success_url = reverse_lazy('posts')
+
+
+class Posts_ListView(ListView):
+    model = Posts
+
+
+class Posts_DetailView(DetailView):
+    model = Posts
+
+
+class Posts_Delete(DeleteView):
+    model = Posts
+    success_url = reverse_lazy('posts')
+
+
+class Users_CreateView(CreateView):
+    model = Users
+    fields = ('username', 'name', 'email')
+    success_url = reverse_lazy('users')
+
+
+class Users_ListView(ListView):
+    model = Users
+
+
+class Users_DetailView(DetailView):
+    model = Users
+
+
+class Users_Delete(DeleteView):
+    model = Users
+    success_url = reverse_lazy('users')
+
+
+class RSS_links_ListView(ListView):
+    model = RSS_links
+
+
+class RSS_links_CreateView(CreateView):
+    model = RSS_links
+    fields = ('name', 'url')
+    success_url = reverse_lazy('sources')
+
+
+class RSS_links_DetailView(DetailView):
+    model = RSS_links
+
+
+class RSS_links_Delete(DeleteView):
+    model = RSS_links
+    success_url = reverse_lazy('sources')
+
+
+class Tags_CreateView(CreateView):
+    model = Tags
+    fields = ('tag', 'users')
+    success_url = reverse_lazy('tags')
+
+
+class Tags_ListView(ListView):
+    model = Tags
+
+
+class Tags_DetailView(DetailView):
+    model = Tags
+
+
+class Tags_Delete(DeleteView):
+    model = Tags
+    success_url = reverse_lazy('tags')
+
+
+class Posts_UpdateView(UpdateView):
+    model = Posts
+    fields = ('title', 'text', 'url', 'from_url')
+    #form_class = PostForm
+    success_url = reverse_lazy('posts')
+
+
+class Users_UpdateView(UpdateView):
+    model = Users
+    fields = ('username', 'name', 'email')
+    success_url = reverse_lazy('users')
+
+class RSS_links_UpdateView(UpdateView):
+    model = RSS_links
+    fields = ('name', 'url')
+    success_url = reverse_lazy('sources')
+
+class Tags_UpdateView(UpdateView):
+    model = Tags
+    fields = ('tag', 'users')
+    success_url = reverse_lazy('tags')
