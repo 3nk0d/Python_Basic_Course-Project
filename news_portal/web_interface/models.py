e9a52a16d29e0a6f5ee0597dc536ac27ef94d8c9
Enk0d <D1man680@yandex.com>
2023-02-11 14:56:51 +0300

    Add user_tags to Users, remove it from Tags, add approved to RSS_links, add additional datetime_string for datetime and post_tags for tags connection

--- news_portal/web_interface/models.py
+++ news_portal/web_interface/models.py
@@ -2,26 +2,34 @@ from django.db import models
 
 # Create your models here.
 
+
+class Tags(models.Model):
+    tag = models.CharField(max_length=50, blank=False, unique=True)
+
+    def __str__(self):
+        return self.tag
+
 class Users(models.Model):
     username = models.CharField(max_length=30, unique=True)
     password = models.CharField(max_length=40)
     name = models.CharField(max_length=50)
     second_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
+    user_tags = models.ManyToManyField(Tags)
 
 
 class RSS_links(models.Model):
     name = models.CharField(max_length=40, unique=True)
+    approved = models.BooleanField()
     link = models.URLField()
 
+    def __str__(self):
+        return self.name
 
 class Posts(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
     source_link = models.URLField()
+    datetime_string = models.CharField(max_length=100)
     rss_link = models.ForeignKey(RSS_links, on_delete=models.PROTECT)
-
-
-class Tags(models.Model):
-    tag = models.CharField(max_length=50, blank=False, unique=True)
-    users_tags = models.ManyToManyField(Users)
+    post_tags = models.ManyToManyField(Tags)
