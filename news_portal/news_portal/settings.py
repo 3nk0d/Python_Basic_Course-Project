6072742cbcc3d71d48105db6b324ffa716aefc7b
Enk0d <D1man680@yandex.com>
2023-02-11 17:43:03 +0300

    Add LOGIN_REDIRECT_URL = '/' LOGOUT_REDIRECT_URL = '/'

--- news_portal/news_portal/settings.py
+++ news_portal/news_portal/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'web_interface',
     'RSS_parser',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#AUTH
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
