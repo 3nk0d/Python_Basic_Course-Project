ac7f8e112a6c5ac3041b3315367acb42f4034e56
Enk0d <D1man680@yandex.com>
2023-02-11 15:05:48 +0300

    Add rss_parser command to parse RSS feeds

---
+++ news_portal/web_interface/management/commands/rss_parser.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand
+import feedparser
+import time
+from web_interface.models import Users, Posts, RSS_links, Tags
+
+class Command (BaseCommand):
+    help = 'RSS parsing'
+
+    def handle(self, *args, **options):
+
+        rss_links = RSS_links.objects.all()
+
+        tags = Tags.objects.all()
+
+        users = Users.objects.all()
+
+        for rss_link in rss_links:
+            print(rss_link.link)
+            response = feedparser.parse(rss_link.link)
+            print(response)
+            if not response['entries']:
+                continue
+            #print(response['entries'])
+            for post in response['entries']:
+                title = post['title']
+                summary = post['summary']
+                source_link = post['link']
+                publish_date_str = post['published']
+                publish_date_time_struct = post['published_parsed']
+                Post = Posts.objects.create(title=title, text=summary, source_link=source_link, datetime_string=publish_date_str, rss_link=rss_link)
+
+                for tag in tags:
+                    if tag.tag in title:
+                        Post.post_tags.add(tag)
+
