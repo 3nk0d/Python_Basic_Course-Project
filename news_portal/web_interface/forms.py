e32e5af1362cea0aef91ca0b14075222ad938da3
Enk0d <D1man680@yandex.com>
2023-02-17 23:02:31 +0300

    Add forms for AddTag_View

--- news_portal/web_interface/forms.py
+++ news_portal/web_interface/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from web_interface.models import RSS_links
+from django.forms import widgets
+from web_interface.models import RSS_links, Subscribe
 
 class RSS_links_AdminCreateForm(forms.ModelForm):
 
@@ -13,3 +14,23 @@ class RSS_links_UserCreateForm(forms.ModelForm):
     class Meta:
         model = RSS_links
         fields = ('name', 'link')
+
+
+class AddTag_View_AdminForm(forms.ModelForm):
+
+    # user_tags = forms.ModelMultipleChoiceField(queryset=Subscribe.objects.all(),
+    #                                            widget=widgets.SelectMultiple(attrs={'size': 20}))
+
+    class Meta:
+        model = Subscribe
+        fields = ('user', 'user_tags')
+
+
+class AddTag_View_UserForm(forms.ModelForm):
+
+    #user_tags = forms.ModelMultipleChoiceField(queryset=Subscribe.objects.all(),
+    #                                          widget=widgets.SelectMultiple(attrs={'size': 20}))
+
+    class Meta:
+        model = Subscribe
+        fields = ('user_tags',)
