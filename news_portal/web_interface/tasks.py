fee9ca89128a33321052ecc187facf0585720fa2
Enk0d <D1man680@yandex.com>
2023-02-23 21:49:17 +0300

    Change exception in tasks.py

--- news_portal/web_interface/tasks.py
+++ news_portal/web_interface/tasks.py
@@ -14,19 +14,19 @@ def rss_parsing():
     for rss_link in rss_links:
         print(rss_link.link)
         response = feedparser.parse(rss_link.link)
-        print(response)
+        #print(response)
         if not response['entries']:
             continue
-        #print(response['entries'])
+        print(response['entries'].len())
         for post in response['entries']:
-            title = post['title']
             try:
+                title = post['title']
                 summary = post['summary']
+                source_link = post['link']
+                publish_date_str = post['published']
+                publish_date_time_struct = post['published_parsed']
             except Exception:
                 continue
-            source_link = post['link']
-            publish_date_str = post['published']
-            publish_date_time_struct = post['published_parsed']
             if Posts.objects.filter(title=title):
                 continue
             Post = Posts.objects.create(title=title, text=summary, source_link=source_link, datetime_string=publish_date_str, rss_link=rss_link)
