fce62284b132190ad3c1ad91ce4ef758b49fa64c
Enk0d <D1man680@yandex.com>
2023-02-25 23:28:45 +0300

    Change database to postgresql

--- news_portal/news_portal/settings.py
+++ news_portal/news_portal/settings.py
@@ -78,8 +78,16 @@ WSGI_APPLICATION = 'news_portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'RSS_News',
+        'USER': 'user',
+        'PASSWORD': 'pass',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
