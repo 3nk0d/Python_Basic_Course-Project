1904fe9f188522846830d898def2fdd3b432451f
Enk0d <D1man680@yandex.com>
2023-02-27 15:59:26 +0300

    Add exception print

--- news_portal/web_interface/tasks.py
+++ news_portal/web_interface/tasks.py
@@ -18,17 +18,29 @@ def rss_parsing():
             continue
         print(len(response['entries']))
         for post in response['entries']:
+
             try:
                 title = post['title']
                 summary = post['summary']
                 source_link = post['link']
                 publish_date_str = post['published']
                 publish_date_time_struct = post['published_parsed']
-            except Exception:
+            except Exception as e:
+                print("Post have an empty field:")
+                print(e)
+                print('Post:')
+                print(post)
                 continue
+
             if Posts.objects.filter(title=title):
                 continue
-            Post = Posts.objects.create(title=title, text=summary, source_link=source_link, datetime_string=publish_date_str, rss_link=rss_link)
+            try:
+                Post = Posts.objects.create(title=title, text=summary, source_link=source_link, datetime_string=publish_date_str, rss_link=rss_link)
+            except Exception as e:
+                print("Post wasn't parsed. Error while write it to DB:")
+                print(e)
+                print('Post:')
+                print(post)
 
             for tag in tags:
                 if (tag.tag in title) or (tag.tag in summary):
